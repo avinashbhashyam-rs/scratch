e49fe41ceed3505f2efdf602daa464a23f297e69
avinashbhashyam-rs <avinash.bhashyam@rightscale.com>
2015-01-16 10:50:30 -0800

    Added input for chef recipe

--- cookbooks/test_cookbook/metadata.rb
+++ cookbooks/test_cookbook/metadata.rb
@@ -6,4 +6,19 @@ description      'see name'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "rightscale"
+
 recipe "test_cookbook::default", "Default recipe for this cookbook."
+
+# Required Input #
+attribute "default/rs_sketchy",
+  :display_name => "rs_sketchy",
+  :description => "sketchy server",
+  :required => "required",
+  :recipes => ["test_cookbook::default"]
+
+attribute "default/rs_tss",
+  :display_name => "rs_tss",
+  :description => "tss server",
+  :required => "required",
+  :recipes => ["test_cookbook::default"]
--- cookbooks/test_cookbook/recipes/default.rb
+++ cookbooks/test_cookbook/recipes/default.rb
@@ -6,6 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+rightscale_marker
+rs_sketchy = node[:default][:rs_sketchy]
+rs_tss = node[:default][:rs_tss]
 
 log "RS_SKETCHY = #{ENV['RS_SKETCHY']}"
 log "RS_TSS = #{ENV['RS_TSS']}"
